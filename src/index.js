684190ef2e5b705d6bf937a06d899c31c67fb495
Jordan Holland <jordanholland21@gmail.com>
2020-10-12 03:38:30 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import './css/style.scss'
+import './css/query.scss'
+import './img/cloud-mountain.jpg'
+import './img/panda.jpg'
+import './img/tie.jpg'
+import './img/doge.jpg'
+import 'animate.css' 
+import Glide from '@glidejs/glide'
+
+
+let glide = new Glide('#showcase', {
+    type: 'carousel',
+    perView: 1,
+    focusAt: 'center',
+    autoplay: 2000,
+    swipeThreshold: 55,
+
+    
+});
+glide.mount();
+var header = document.getElementById('id');
+var stickpoint = header.offsetTop;
+window.addEventListener('scroll',function(){
+    if ((window.pageYOffset >= stickpoint)){
+
+        header.classList.add('sticky');
+    } else if ((window.pageYOffset < stickpoint)) {
+        header.classList.remove('sticky');
+       
+    }
+});
+
+var year = new Date().getFullYear();
+var date =`&copy; EtherealCoder ${year}`
+document.getElementById('copyright').innerHTML = date;
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.[contenthash].js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,'css-loader','sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/env'],
+                    }
+                }  
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name:'[name][contenthash].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                            
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            title: 'Ethereal Coder',
+            meta: {
+                description: 'Ethereal Coders Portfolio'
+            }
+        }),
+        new TerserPlugin(), 
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+            
+        }),
+        new CleanWebpackPlugin()
+
+    ]
+}
